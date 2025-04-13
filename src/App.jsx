40fc4cfd4a07b6bd70438dd64b47bf50d8f83ad8
Chris Miles <cmiles@obm.com>
2025-04-13 14:15:44 -0400

    mouse spotlight

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { useSectionVisibility } from "./hooks/useSectionVisibility";
+import Spotlight from "./components/Spotlight";
 
 const App = () => {
   const activeSection = useSectionVisibility();
 
   return (
     <div className="bg-slate-900 text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+      <Spotlight />
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
